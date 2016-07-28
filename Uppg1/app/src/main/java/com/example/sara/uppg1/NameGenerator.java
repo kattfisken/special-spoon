79bab0b3f2c1932edd83f95b2a8d2457206d5edd
Ludvig Hult <ludvig.hult@gmail.com>
2016-07-28 11:52:39 +0200

    Uppgiftt 1 är nu klar

--- Uppg1/app/src/main/java/com/example/sara/uppg1/NameGenerator.java
+++ Uppg1/app/src/main/java/com/example/sara/uppg1/NameGenerator.java
@@ -1,11 +1,14 @@
 package com.example.sara.uppg1;
 
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -20,11 +23,12 @@ public class NameGenerator extends AppCompatActivity {
 
     public ArrayAdapter<String> nameListArrayAdapter;
     private final String LOGTAG = "Uppgift 1 Custom log";
-    private final String[] nameRepo = {"Ludvig", "Kalle", "Pelle", "Nisse", "Jan", "Karl", "Carl-Gustaf", "Gustaf", "Gustav"};
+    private String[] static_array_of_names;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        static_array_of_names = getResources().getStringArray(R.array.long_list_of_names);
 
 
         ArrayList<String> defaultNameList = new ArrayList<String>();
@@ -38,52 +42,81 @@ public class NameGenerator extends AppCompatActivity {
         ListView nameList = (ListView)findViewById(R.id.name_list);
         if (nameList != null) {
             nameList.setAdapter(nameListArrayAdapter);
+            nameList.setOnItemClickListener(new NameDeleteListener(nameListArrayAdapter));
         } else {
-            android.util.Log.e(LOGTAG,"listView har försvunnit");
+            android.util.Log.e(LOGTAG,"the listView has disappeared");
         }
 
         for(int i=1; i<=3;i++){
             addRandomName();
         }
 
-        nameList.setOnItemClickListener(
-                new AdapterView.OnItemClickListener()  {
-                    @Override
-                    public void onItemClick(AdapterView<?> adapter, View v, int position, long arg3){
-                        nameListArrayAdapter.remove(nameListArrayAdapter.getItem(position));
-                    }
-                }
-
-
-        );
 
     }
+
+    class NameDeleteListener implements AdapterView.OnItemClickListener {
+
+        private ArrayAdapter<String> arrayAdapter;
+
+
+        public NameDeleteListener(ArrayAdapter<String> input_aa) {
+            arrayAdapter = input_aa;
+        }
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+
+
+
+            AlertDialog.Builder builder =
+                    new AlertDialog.Builder(NameGenerator.this);
+            builder.setCancelable(true);
+            builder.setMessage(R.string.delete_dialog_message);
+            builder.setPositiveButton(R.string.delete_dialog_positive, new DialogInterface.OnClickListener() {
+                int position_to_delete = position;
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    arrayAdapter.remove(arrayAdapter.getItem(position));
+                }
+            });
+            builder.setNegativeButton(R.string.delete_dialog_negative, null);
+            builder.show();
+
+        }
+    }
+
+
     public void addRandomName(View v) {
         addRandomName();
     }
 
     public void addRandomName() {
-        int rnd = new Random().nextInt(nameRepo.length);
-        String randomName = nameRepo[rnd];
+        int my_random_int;
+        String randomName;
+        int n_try;
+        boolean name_already_in_list;
+        View root_view;
+
+        my_random_int = new Random().nextInt(static_array_of_names.length);
+        randomName = static_array_of_names[my_random_int];
+        n_try = 0;
+
+        name_already_in_list = true;
+        while ((n_try < static_array_of_names.length) && name_already_in_list) {
 
 
+            n_try++; // for try number x....
+            randomName = static_array_of_names[my_random_int]; // pick a new name to try
+            name_already_in_list = false; // and this name, have not yet been found in the list of displayed names
 
-        //leta igenom hela listan
-        //om den är i listan så ta rnd++; och börja om letandet
-        // om rnd > listan är lång så skall man gå till första namnet
-        // om alla namn prövats, så ge ett felmeddelande (toast?)
-        int n_try = 0;
-        boolean name_already_in_list = true;
-        while ((n_try < nameRepo.length) && name_already_in_list) {
-            randomName = nameRepo[rnd];
-            n_try++;
-            name_already_in_list = false; // assume it is not in the list....
+
             for (int try_pos=0;try_pos<nameListArrayAdapter.getCount();try_pos++) {
+
                 // but if we find it when looking for it...
                 if (randomName.equals(nameListArrayAdapter.getItem(try_pos))) {
                     //try with next name from the repo
-                    rnd++;
-                    rnd = rnd % nameRepo.length;
+                    my_random_int++;
+                    my_random_int = my_random_int % static_array_of_names.length;
                     name_already_in_list = true;
                     break;
                 }
@@ -93,11 +126,17 @@ public class NameGenerator extends AppCompatActivity {
         // så vid scannandet och slumpandets slut...
         if (name_already_in_list) {
             android.util.Log.e(LOGTAG,"Name already in the list.");
-            Snackbar sb = Snackbar.make(
-                    findViewById(R.id.root_layout)
-                    ,R.string.no_more_names_snackbar
-                    ,Snackbar.LENGTH_SHORT);
-            sb.show();
+            Snackbar out_of_names_error_snackbar;
+            root_view = findViewById(R.id.root_layout);
+            if ( root_view != null) {
+                out_of_names_error_snackbar = Snackbar.make(
+                        root_view
+                        , R.string.no_more_names_snackbar
+                        , Snackbar.LENGTH_SHORT);
+                out_of_names_error_snackbar.show();
+            }else {
+                android.util.Log.e(LOGTAG,"The root view cannot be obtained");
+            }
 
         } else {
             nameListArrayAdapter.add(randomName);
