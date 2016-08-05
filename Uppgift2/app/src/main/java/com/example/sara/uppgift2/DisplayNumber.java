ae3e9228810e907199099c9f19e8d0eadb8f4ec6
Ludvig Hult <ludvig.hult@gmail.com>
2016-08-05 23:11:15 +0200

    kosmetiska ändringar i kod

--- Uppgift2/app/src/main/java/com/example/sara/uppgift2/DisplayNumber.java
+++ Uppgift2/app/src/main/java/com/example/sara/uppgift2/DisplayNumber.java
@@ -46,6 +46,7 @@ public class DisplayNumber extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_number);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
