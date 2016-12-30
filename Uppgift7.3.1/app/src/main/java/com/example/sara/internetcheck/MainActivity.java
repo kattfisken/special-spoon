2b3ac07e7fa0b796b117b239abe82a6078b37734
Sara Lidbaum <sara.lidbaum@gmail.com>
2016-12-30 20:24:27 +0100

    Uppgift 7.3.1 klar och kommenterad!

--- Uppgift7.3.1/app/src/main/java/com/example/sara/internetcheck/MainActivity.java
+++ Uppgift7.3.1/app/src/main/java/com/example/sara/internetcheck/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
+import android.os.Build;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,59 +17,110 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Calendar;
 
+/**
+ * An activity that shows a screen. First the screen is yellow saying "maybe" about connection
+ * status but after a short while it says "yes" or "no" depending on connection status.
+ */
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = "InternetTest";
 
+    public enum ConnectionStatus {GOOD, BAD, NEUTRAL}
+
+
+    /**
+     * set the GUI and update the connection status report in the main screen.
+     *
+     * @param savedInstanceState not in use.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        updateClickAction(findViewById(R.id.root_element));
+        updateClickAction(null);
     }
 
-    public void updateClickAction(View v) {
+    /**
+     * Set the screen to neutral state (neutral color and "maybe" text) and then re-query if we have
+     * connection to internet.
+     *
+     * @param v not used. Only for XML onClick registration. May be Null.
+     */
+    public void updateClickAction(@Nullable View v) {
 
-        //restore to neutral
-        ((TextView) findViewById(R.id.text_header)).setText(R.string.neutral_news_title);
-        String s = String.format(getString(R.string.neutral_news_text), "The check is not done yet.");
-        (findViewById(R.id.root_element)).setBackgroundColor(getResources().getColor(R.color.indicator_neutral));
+        //restore header to neutral
+        updateTextAndColor(ConnectionStatus.NEUTRAL);
 
+        // ask background method to check the internet connection
         new CheckWithGoogleTask().execute(this);
     }
 
-    public void updateTextAndColor(boolean b) {
+    /**
+     * Sets the background color and the text in the main window depending on input connectionStatus.
+     *
+     * @param connectionStatus Enum values good, bad or neutral.
+     */
+    public void updateTextAndColor(ConnectionStatus connectionStatus) {
         Calendar c = Calendar.getInstance();
 
-        int headerR = R.string.bad_news_title; // bad as default
-        int bodyR = R.string.bad_news_text;
-        int colorR = R.color.indicator_bad;
+        int headerResourceId;
+        int textResourceId;
+        int colorResourceId;
+        String s;
 
-        if (b) { //potentially change to good
-            headerR = R.string.good_news_title;
-            bodyR = R.string.good_news_text;
-            colorR = R.color.indicator_good;
+        switch (connectionStatus) {
+            case BAD:
+                headerResourceId = R.string.bad_news_title;
+                textResourceId = R.string.bad_news_text;
+                colorResourceId = R.color.indicator_bad;
+                s = String.format(getString(textResourceId), c);
+                break;
+            case GOOD:
+                headerResourceId = R.string.good_news_title;
+                textResourceId = R.string.good_news_text;
+                colorResourceId = R.color.indicator_good;
+                s = String.format(getString(textResourceId), c);
+                break;
+            case NEUTRAL:
+            default:
+                headerResourceId = R.string.neutral_news_title;
+                textResourceId = R.string.neutral_news_text;
+                colorResourceId = R.color.indicator_neutral;
+                s = String.format(getString(textResourceId), "The check is not done yet.");
+                break;
+
         }
 
-        String s = String.format(getString(bodyR), c);
         ((TextView) findViewById(R.id.text_body)).setText(s);
-        ((TextView) findViewById(R.id.text_header)).setText(headerR);
-        (findViewById(R.id.root_element)).setBackgroundColor(getResources().getColor(colorR));
+        ((TextView) findViewById(R.id.text_header)).setText(headerResourceId);
+        int color;
+        if (Build.VERSION.SDK_INT >= 23) {
+            color = getResources().getColor(colorResourceId, null);
+        } else {
+            //noinspection deprecation
+            color = getResources().getColor(colorResourceId);
+        }
+        (findViewById(R.id.root_element)).setBackgroundColor(color);
     }
 
+    /**
+     * Class for making http response to google asynchronously.
+     */
     private class CheckWithGoogleTask extends AsyncTask<Context, Integer, Boolean> {
 
         /**
-         * Check if the host www.google.com can be reached over the internet connections
-         * requires the permission INTERNET
-         * @param context the app context
+         * Check if the host www.google.com can be reached over the internet connections.
+         * Requires the permission INTERNET.
+         *
+         * @param context The app context.
          * @return true/false depending if www.google.com can deliver a HTTP response code 200
          */
-        public boolean hasActiveInternetConnection(Context context) {
+        boolean hasActiveInternetConnection(Context context) {
             if (isNetworkAvailable(context)) {
                 try {
-                    HttpURLConnection urlc = (HttpURLConnection) (new URL("http://www.google.com").openConnection());
+                    HttpURLConnection urlc = (HttpURLConnection) (new URL("http://www.google.com")
+                            .openConnection());
                     urlc.setRequestProperty("User-Agent", "Test");
                     urlc.setRequestProperty("Connection", "close");
                     urlc.setConnectTimeout(1500);
@@ -84,14 +137,15 @@ public class MainActivity extends AppCompatActivity {
         }
 
         /**
-         * check if there is any connected network. uses permission "ACCESS_NETWORK_STATE".
-         * does not check for internet connection.
+         * Check if there is any connected network. uses permission "ACCESS_NETWORK_STATE".
+         * Does not check for <em>internet</em> connection - only network connection.
          *
-         * @param context the context for the app
-         * @return true/false depending on netowrk access
+         * @param context the context for the app.
+         * @return true/false depending on network access.
          */
-        public boolean isNetworkAvailable(Context context) {
-            ConnectivityManager connectivity = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        boolean isNetworkAvailable(Context context) {
+            ConnectivityManager connectivity =
+                    (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
 
             //is null for example when in airplane mode
             if (connectivity == null) {
@@ -103,8 +157,8 @@ public class MainActivity extends AppCompatActivity {
                 //noinspection deprecation
                 NetworkInfo[] info = connectivity.getAllNetworkInfo();
                 if (info != null) {
-                    for (int i = 0; i < info.length; i++) {
-                        if (info[i].getState() == NetworkInfo.State.CONNECTED) {
+                    for (NetworkInfo anInfo : info) {
+                        if (anInfo.getState() == NetworkInfo.State.CONNECTED) {
                             return true;
                         }
                     }
@@ -116,26 +170,33 @@ public class MainActivity extends AppCompatActivity {
         }
 
         /**
-         * Override this method to perform a computation on a background thread. The
+         * This method performs a computation on a background thread. The
          * specified parameters are the parameters passed to {@link #execute}
          * by the caller of this task.
          * <p>
-         * This method can call {@link #publishProgress} to publish updates
-         * on the UI thread.
+         * When finished, this method calls {@link #onPostExecute} in the GUI thread.
          *
          * @param params The context of the activity calling the async task
          * @return true/false depending on whether there is a 200 response from Google.com
-         * @see #onPreExecute()
          * @see #onPostExecute
-         * @see #publishProgress
          */
         @Override
         protected Boolean doInBackground(Context... params) {
             return hasActiveInternetConnection(params[0]);
         }
 
+        /**
+         * Update the GUI according to result in asynchronous task.
+         * This task is run automatically when async task is done.
+         *
+         * @param result True if we have internet connection. Otherwise False.
+         */
         protected void onPostExecute(Boolean result) {
-            MainActivity.this.updateTextAndColor(result);
+            if (result) {
+                MainActivity.this.updateTextAndColor(ConnectionStatus.GOOD);
+            } else {
+                MainActivity.this.updateTextAndColor(ConnectionStatus.BAD);
+            }
         }
     }
 
